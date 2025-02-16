f9e8890a0f2714d6cd2e0df2b107504b1b994212
Monday1997 <axu1997@qq.com>
2025-02-16 17:28:43 +0800

    feat: 自动导入

--- src/pages/main/user/[id].vue
+++ src/pages/main/user/[id].vue
@@ -1,11 +1,12 @@
 <template>
   <div>{{ route.params.id }}</div>
+  <div>{{ data1 }}</div>
 </template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
 const route = useRoute('/user/[id]')
 console.log(route.params.id)
+const data1 = ref(13)
 </script>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,28 +5,42 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     VueRouter({
-      routesFolder:[
-       {
-        src:'src/pages/main'
-       },
-       {
-        src:'src/pages/admin',
-        path:'admin/'
-       }
+      routesFolder: [
+        {
+          src: 'src/pages/main',
+        },
+        {
+          src: 'src/pages/admin',
+          path: 'admin/',
+        },
       ],
-      exclude:['**/components/**']
+      exclude: ['**/components/**'],
     }),
     vue(),
     vueJsx(),
     vueDevTools(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: [
+        // presets
+        'vue',
+        'vue-router',
+      ],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
